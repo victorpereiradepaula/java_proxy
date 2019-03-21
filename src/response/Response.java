c6b1a03700a4db668217efa45b7e06341730809d
Victor Pereira <victorpereira@jera.com.br>
2019-03-21 17:45:38 -0400

    fixed

--- src/response/Response.java
+++ src/response/Response.java
@@ -21,12 +21,12 @@ public final class Response implements ResponseInterface {
 			break;
 
 			case 404:
-				response += "HTTP/1.1 404 File Not Found\r\n";
+				response += "HTTP/1.1 200 File OK\r\n";
 				responseHtml = getErrorPage(statusCode, "File Not Found");
 			break;				
 
 			default:
-				response += String.format("HTTP/1.1 %d Error\r\n", statusCode);
+				response += String.format("HTTP/1.1 200 OK\r\n");
 				responseHtml = getErrorPage(statusCode, "Error");
 				break;
 		}
@@ -41,6 +41,6 @@ public final class Response implements ResponseInterface {
 	}
 
 	private final String getErrorPage(int error, String description) {
-    return "<!DOCTYPE html><html lang=\"pt-br\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\"><title>Error Page</title></head><body><div style=\"text-align: center\"><h1>Ops, não conseguimos acessar essa página</h1><img src=\"https://32jn1p2jfust2jm6d92xtg5d-wpengine.netdna-ssl.com/wp-content/uploads/2014/10/Leaking-Pipe.jpg\" alt=\"Error image\"><p>Experimente sem o proxy, ele ainda esta em sua fase experimental...</p><div style=\"display: inline-block; width: 45%\"><h2 style=\"font-size: 50px; margin: 0\">" + error + "</h2></div><div style=\"display: inline-block; width: 45%\"><p style=\"text-align: justify; margin: 0;padding-right: 18%\">" + description + "</p></div></div></body></html>";
+    return "<!DOCTYPE html><html lang=\"pt-br\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\"><title>Proxy Error Page</title></head><body><div style=\"text-align: center\"><h1>Ops, algo deu errado...</h1><img src=\"https://32jn1p2jfust2jm6d92xtg5d-wpengine.netdna-ssl.com/wp-content/uploads/2014/10/Leaking-Pipe.jpg\" alt=\"Error image\"><p>Experimente sem o proxy, ele ainda esta em sua fase experimental...</p><div style=\"display: inline-block; width: 45%\"><h2 style=\"font-size: 50px; margin: 0\">" + error + "</h2></div><div style=\"display: inline-block; width: 45%\"><p style=\"text-align: justify; margin: 0;padding-right: 18%\">" + description + "</p></div></div></body></html>";
   }
 }
